720379a7be174db9a0d39093e7b691e3e49a6721
Partha Sarathy s <sasisarathy56@gmail.com>
2025-08-07 18:08:37 +0530

    Add files via upload

--- email.js
+++ email.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.ALERT_EMAIL_USER,
-    pass: process.env.ALERT_EMAIL_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
 function sendAlert(batteryVoltage) {
   const mailOptions = {
     from: `"Voltage Monitor" <${process.env.ALERT_EMAIL_USER}>`,
-    to: process.env.ALERT_EMAIL_TO,
+    to: process.env.EMAIL_RECEIVER,
     subject: '⚠️ Low Battery Voltage Alert',
     text: `Battery voltage is too low: ${batteryVoltage}V.\nPlease take necessary action.`,
     html: `<h3>⚠️ Low Battery Voltage Alert</h3><p>Battery voltage is too low: <b>${batteryVoltage}V</b>.</p><p>Please check your system.</p>`
